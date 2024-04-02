d1e3e6e836b89b4bbf5fb3b863b6fa9614340269
Jakub Hrncar <jakubko.hrncar@gmail.com>
2024-04-02 11:48:28 +0200

    ambulance waiting list

--- cmd/ambulance-api-service/main.go
+++ cmd/ambulance-api-service/main.go
@@ -8,22 +8,45 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jhrncar/ambulance-webapi/api"
 	"github.com/jhrncar/ambulance-webapi/internal/ambulance_wl"
+
+	"context"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/jhrncar/ambulance-webapi/internal/db_service"
 )
 
 func main() {
-    log.Printf("Server started")
-    port := os.Getenv("AMBULANCE_API_PORT")
-    if port == "" {
-        port = "8080"
-    }
-    environment := os.Getenv("AMBULANCE_API_ENVIRONMENT")
-    if !strings.EqualFold(environment, "production") { // case insensitive comparison
-        gin.SetMode(gin.DebugMode)
-    }
-    engine := gin.New()
-    engine.Use(gin.Recovery())
-    // request routings
+	log.Printf("Server started")
+	port := os.Getenv("AMBULANCE_API_PORT")
+	if port == "" {
+		port = "8080"
+	}
+	environment := os.Getenv("AMBULANCE_API_ENVIRONMENT")
+	if !strings.EqualFold(environment, "production") { // case insensitive comparison
+		gin.SetMode(gin.DebugMode)
+	}
+	engine := gin.New()
+	engine.Use(gin.Recovery())
+	corsMiddleware := cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE", "PATCH"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
+		ExposeHeaders:    []string{""},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+	})
+	engine.Use(corsMiddleware)
+
+	// setup context update  middleware
+	dbService := db_service.NewMongoService[ambulance_wl.Ambulance](db_service.MongoServiceConfig{})
+	defer dbService.Disconnect(context.Background())
+	engine.Use(func(ctx *gin.Context) {
+		ctx.Set("db_service", dbService)
+		ctx.Next()
+	})
+	// request routings
 	ambulance_wl.AddRoutes(engine)
-    engine.GET("/openapi", api.HandleOpenApi)
-    engine.Run(":" + port)
+	engine.GET("/openapi", api.HandleOpenApi)
+	engine.Run(":" + port)
 }
